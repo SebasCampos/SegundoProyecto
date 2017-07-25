4b158819708ae02ea9976e64ec9cb6ed303d6266
Kendall <Kendall@LAPTOP-I5M9NSJ9>
2017-07-24 19:42:25 -0600

    ya logre ingresar

---
+++ src/segundo_programacion_juegos/Segundo_Programacion_Juegos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundo_programacion_juegos;
+
+/**
+ *
+ * @author Kendall
+ */
+public class Segundo_Programacion_Juegos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
